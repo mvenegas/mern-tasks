7375434e1a714c9cf809a3046f6baa72ebbddca5
Michael Venegas <lim_venegas@hotmail.com>
2021-10-16 00:00:02 -0500

    mLab ha sido agregado

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const URI='mongodb://localhost/mern-tasks';
+//const URI='mongodb://localhost/mern-tasks';
+const URI='mongodb+srv://mvenegas:lapdmvs1@cluster0.cafgg.mongodb.net/mern-tasks';
+
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
